5b098e294fa5b470a42a6cb77c5e6d25fe250e07
unknown <dasilvadeoliveirafabio@gmail.com>
2022-08-13 20:05:07 -0300

    teste de deploy 10

--- Rotas.js
+++ Rotas.js
@@ -3,6 +3,9 @@ const rota = require('express').Router()
 const multer  = require('multer')
 const upload = multer()
 const controllerUsuario = require('./Controllers/controllerUsuario')
+rota.get('/',(req,res)=>{
+    res.send('<h1>back-end rodando ...</h1>')
+})
 rota.get('/usuarioslist',controllerUsuario.getUsuarios)
 rota.post('/jwt',upload.single('avatar'),controllerUsuario.autenticarCliente)
 module.exports = rota
