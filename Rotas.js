b1206a219a77b6a14e929b242647758a49a836dc
unknown <dasilvadeoliveirafabio@gmail.com>
2022-08-13 19:54:05 -0300

    teste de deploy 9

--- Rotas.js
+++ Rotas.js
@@ -1,7 +1,7 @@
 const rota = require('express').Router()
 
 const multer  = require('multer')
-const upload = multer({ dest: './public/data/uploads/' })
+const upload = multer({ dest: './uploads' })
 const controllerUsuario = require('./Controllers/controllerUsuario')
 rota.get('/usuarioslist',controllerUsuario.getUsuarios)
 rota.post('/jwt',upload.single('avatar'),controllerUsuario.autenticarCliente)
