78b6a1946f4f3e065077a43920e7d7be017b42c9
unknown <dasilvadeoliveirafabio@gmail.com>
2022-08-13 18:54:55 -0300

    teste de deploy 3

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const conexao = require('./conexao')
 conexao()
 const app = express()
 app.get('/',(req,res)=>{
-    res.send('<h1>back end rodando ...</h1>')
+    res.send('<h1>back end rodando ok ...</h1>')
 })
 app.listen('4000')
