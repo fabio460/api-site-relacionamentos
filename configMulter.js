4125a1566c7737ccd3b8c0de7cab82af0b25a5d4
unknown <dasilvadeoliveirafabio@gmail.com>
2022-10-06 17:29:32 -0300

    cadastro criado

--- configMulter.js
+++ configMulter.js
@@ -4,14 +4,14 @@ var multer = require('multer')
 var multerS3 = require('multer-s3')
 const crypto = require('crypto')
 aws.config.update({
-    accessKeyId:'AKIA2QECU5ZFBMIBO3WY',
-    secretAccessKey:'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',
-    region:'us-east-1'
+    accessKeyId:process.env.ACCESS_KEY,
+    secretAccessKey:process.env.ACCESS_SECRET,
+    region:process.env.REGION
 })
 var upload = multer({
   storage: multerS3({
     s3: new aws.S3(),
-    bucket: 'imagensperfi',
+    bucket: process.env.BUCKET,
     acl: 'public-read',
     contentType: multerS3.AUTO_CONTENT_TYPE,
     key: function (req, file, cb) {
