1253edb96b437a0b9fcd0ce3aa7f47f99fa92485
unknown <dasilvadeoliveirafabio@gmail.com>
2022-10-11 14:28:14 -0300

    delete e update criados

--- Controllers/controllerUsuario.js
+++ Controllers/controllerUsuario.js
@@ -1,5 +1,7 @@
 const modelUsuario = require('../Models/modelUsuario')
 const jwt = require('jsonwebtoken')
+var aws = require('aws-sdk')
+require('dotenv').config()
 exports.getUsuarios =async (req,res)=>{
     const p = await modelUsuario.find()
     res.send(p)
@@ -31,6 +33,66 @@ exports.setUsuarios = (req,res)=>{
   
   res.send('ss')
 }
+
+exports.updateUsuario = (req,res)=>{
+  var imagem = ''
+  if (req.file) {
+    imagem = req.file.location
+  }
+   try {
+    modelUsuario.findByIdAndUpdate(req.body.id,{
+      nome:req.body.nome,
+      email:req.body.email,
+      senha:req.body.senha,
+      imagemPerfil:imagem,
+      cidade:req.body.cidade,
+      bairro:req.body.bairro,
+      estado:req.body.estado,
+      logradouro:req.body.logradouro,
+      cep:req.body.cep,
+      rua:req.body.rua,
+      complemento:req.body.complemento,
+  
+      profissao:req.body.profissao,
+      telefone:req.body.telefone,
+      observacoesFinais:req.body.observacoesFinais,
+      outrasHabilidades:req.body.outrasHabilidades,
+    },(err)=>{
+      if (err) {
+        res.send(err)
+      } else {
+        res.send('atualizado com sucesso')
+      }
+    })
+    
+   } catch (error) {
+    res.send(error)
+   }
+}
+
+exports.removerConta =async (req,res)=>{
+  let s3=new aws.S3()
+  const post =await modelUsuario.find({
+      _id:req.body.id
+  })
+   
+   let objeto = post[0].imagemPerfil
+   let objetoArray = objeto.split('/')
+   let fileName = objetoArray[3]
+  try {
+      const deletar = modelUsuario.findByIdAndDelete(req.body.id,err=>{
+          if (err) {
+              res.send(err)
+          } else {
+              s3.deleteObject({ Bucket: process.env.BUCKET, Key:fileName}).promise();
+              res.send('deletado com sucesso')
+          }
+      })
+  } catch (error) {
+      console.log(error)
+  }
+}
+
 
 exports.autenticarCliente = async (req,res)=>{
   const {email,senha} = req.body;
--- Rotas.js
+++ Rotas.js
@@ -9,4 +9,7 @@ rota.get('/',(req,res)=>{
 rota.get('/getUsuarios',controllerUsuario.getUsuarios)
 rota.post('/jwt',upload.single('avatar'),controllerUsuario.autenticarCliente)
 rota.post('/setUsuario',upload.single('imagemPerfil'),controllerUsuario.setUsuarios)
+
+rota.put('/updateUsuario',upload.single('imagemPerfil'),controllerUsuario.updateUsuario)
+rota.delete('/removerConta',upload.single('imagemPerfil'),controllerUsuario.removerConta)
 module.exports = rota
