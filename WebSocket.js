4255b847b73efc4b8e881479e1541cb280b5bccf
unknown <dasilvadeoliveirafabio@gmail.com>
2022-10-17 14:00:41 -0300

    websocket implementado

--- WebSocket.js
+++ WebSocket.js
@@ -5,8 +5,12 @@ const modelChat = require('./Models/modelChat')
 const  WebSocket = (app)=> {
     const server = http.createServer(app);
     const io = new Server(server, {
+        // cors: {
+        //     origin: "http://localhost:3000",
+        //     methods: ["GET", "POST"],
+        // },
         cors: {
-            origin: "http://localhost:3000",
+            origin: "https://site-relacionamentos-fabio460.vercel.app",
             methods: ["GET", "POST"],
         },
     });
