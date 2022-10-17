a89073953d3e32ccda656a875695cdc6d75d0d5b
unknown <dasilvadeoliveirafabio@gmail.com>
2022-10-17 13:36:14 -0300

    websocket implementado

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express')
 require('dotenv').config()
+const cors = require("cors");
 const conexao = require('./conexao')
 conexao()
 const rota = require('./Rotas')
 const app = express()
-
-app.use(rota)
-const cors = require("cors");
 app.use(cors());
+app.use(rota)
+
+
 const WebSocket = require('./WebSocket')
 app.listen(4000, () => {
   console.log("Servidor rodando...");
